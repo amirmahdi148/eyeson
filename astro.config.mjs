a500187281a4ac38b6dfb5bc44d9487562d3a933
am1r <amir9580mahdi110@gmail.com>
2026-06-25 18:31:28 +0330

    feat: add new vectors
    add new vectors to uiux and branding page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
-import node from '@astrojs/node';
 import vercelAdapter from "@astrojs/vercel";
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://eyeson.agency',
   output: 'server',
-  // adapter: node({ mode: 'standalone' }),
   adapter: vercelAdapter(),
   integrations: [react(), svelte(), sitemap({
     filter: (page) => !page.startsWith('https://eyeson.agency/admin/') && page !== 'https://eyeson.agency/login/',
   })],
   vite: {
+    plugins: [tailwindcss()],
     build: {
       chunkSizeWarningLimit: 1000,
     },
